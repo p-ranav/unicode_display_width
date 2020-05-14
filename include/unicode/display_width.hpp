b4b13fe8f078d810b2089faa7b89738f644c448b
Pranav <pranav.srinivas.kumar@gmail.com>
2020-05-14 13:26:25 -0500

    Removed Windows specific code

--- include/unicode/display_width.hpp
+++ include/unicode/display_width.hpp
@@ -4,11 +4,7 @@
 #include <clocale>
 #include <locale>
 #include <cstdlib>
-#if defined(__unix__) || defined(__unix) || defined(__APPLE__)
 #include <codecvt>
-#else 
-#include <Windows.h>
-#endif
 
 namespace unicode {
 
@@ -322,7 +318,6 @@ int mk_wcswidth_cjk(const wchar_t* pwcs, size_t n)
   return width;
 }
 
-#if defined(__unix__) || defined(__unix) || defined(__APPLE__)
 // convert UTF-8 string to wstring
 std::wstring utf8_decode(const std::string& str) {
   std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
@@ -334,27 +329,6 @@ std::string utf8_encode(const std::wstring& str) {
   std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
   return myconv.to_bytes(str);
 }
-#else
-// Convert a wide Unicode string to an UTF8 string
-std::string utf8_encode(const std::wstring& wstr)
-{
-  if (wstr.empty()) return std::string();
-  int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
-  std::string strTo(size_needed, 0);
-  WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
-  return strTo;
-}
-
-// Convert an UTF8 string to a wide Unicode String
-std::wstring utf8_decode(const std::string& str)
-{
-  if (str.empty()) return std::wstring();
-  int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
-  std::wstring wstrTo(size_needed, 0);
-  MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
-  return wstrTo;
-}
-#endif
 
 }
 
